42f08bd75aea91496f5a2b00afdb41fa0a6a6387
Domingo Gallardo <domingo.gallardo@ua.es>
2022-09-27 09:15:11 +0200

    Recogemos el número con un formulario

---
+++ src/main/java/demoapp/controller/NumData.java
@@ -0,0 +1,13 @@
+package demoapp.controller;
+
+public class NumData {
+    Integer numero;
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+}
--- src/main/java/demoapp/controller/ParController.java
+++ src/main/java/demoapp/controller/ParController.java
@@ -3,9 +3,8 @@ package demoapp.controller;
 import demoapp.service.ParService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ParController {
@@ -13,10 +12,15 @@ public class ParController {
     @Autowired
     private ParService service;
 
-    @GetMapping("/esPar/{numero}")
-    public @ResponseBody String esPar(@PathVariable(value="numero") String numero) {
-        Integer num = new Integer(numero);
-        String resultado = "El número " + numero + (service.esPar(num) ? " es par" : " no es par");
+    @GetMapping("/esPar")
+    // Hay que declarar un parámetro con el tipo usado en el modelo del formulario (UserData)
+    public String saludoForm(NumData numData) {
+        return "formGetNum";
+    }
+
+    @PostMapping("/esPar")
+    public @ResponseBody String esPar(@ModelAttribute NumData numero, Model model) {
+        String resultado = "El número " + numero.getNumero() + (service.esPar(numero.getNumero()) ? " es par" : " no es par");
         return resultado;
     }
 }
